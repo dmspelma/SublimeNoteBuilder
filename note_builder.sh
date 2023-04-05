fbdba80d7ff00015fff07231bdfc0f31b4c88893
Drew Spelman <drewspelman@gmail.com>
2023-04-04 18:34:56 -0700

    Add functionality for creating a dev note, handle case where folder for notes don't exist, add filter function to handle symbols in trello card name

--- note_builder.sh
+++ note_builder.sh
@@ -2,24 +2,52 @@
 
 default_path=$HOME
 path_extension='Documents/Notes'
-echo "Building a new note for you under $default_path$path_extension"
 formatted_date=$(date +%m/%d/%Y)
+
+
+while getopts ":d" opt; do
+  case ${opt} in
+    d )
+      path_extension='Documents/Notes/DevNotes'
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+full_path="$default_path/$path_extension"
+echo "Building a new note for you under $full_path"
+
+if [ ! -d "$full_path" ]; then
+	# -p ensure the directory and parent directories are created if they don't exist
+  mkdir -p "$full_path"
+  echo "Created directory $full_path"
+fi
 
 # For given name, substitutes all spaces with _ and appends `.notes` file extension
 function note_name(){
-	tmp=$*
+	tmp=$1
 	tmp="${tmp// /_}"
 	echo "$tmp.notes"
 }
 
+function filter_field(){
+	local=$1
+	filtered="${local//[^a-zA-Z0-9_-]/}"
+	echo "$filtered"
+}
+
 read -p "Trello Card Title: " card_title
 read -p "Trello Link: " card_link
-note_file="$default_path/$path_extension/$(note_name $card_title)"
+note_file="$full_path/$(note_name "$(filter_field "$card_title")")"
 echo "Note is saved here: $note_file"
 
- # Create file and fill with template. Will not create file if one exists
-if [ -e "${note_file[@]}" ]; then
+# Create file and fill with template. Will not create file if one exists
+if [ -e "${note_file}" ]; then # -e flag checks if file exists in given path
   echo "File ${note_file} already exists!"
+  exit 1
 else
 	cat > "${note_file}" <<EOF
 Trello Card: $card_title
